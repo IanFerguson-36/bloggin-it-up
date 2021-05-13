41401e52b071f7b2dadd45cfa7fc8adf83371490
Ian F <ian.ferg36@gmail.com>
2021-05-12 18:17:31 -0600

    added homeroutes to the server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
+const homeRoutes = require("./controllers/homeroutes.js");
 
 const app = express();
 const PORT = process.env.PORT || 3331;
@@ -15,6 +16,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(homeRoutes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening `));
 });
